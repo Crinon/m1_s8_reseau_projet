352712a4bbca132f7be89c5f7084c54f83079721
Nicolas Crinon <48944000+Crinon@users.noreply.github.com>
2021-05-17 23:06:41 +0200

    Code fonctionnel

--- src/main/java/fr/ul/miage/ownhttpserver/Main.java
+++ src/main/java/fr/ul/miage/ownhttpserver/Main.java
@@ -8,6 +8,7 @@ import java.net.URISyntaxException;
 
 public class Main {
 	static final int port = 4006;
+	
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		InetAddress host = InetAddress.getByName("0.0.0.0");
     	System.out.println("Main launched with port "+port);
@@ -16,6 +17,9 @@ public class Main {
         	Socket socket = srv.accept();
         	OwnHttpServer myownserver = new OwnHttpServer(socket, "maxi web");
             System.out.println("Server started on port "+port);
+            
+    		Thread thread = new Thread(myownserver);
+    		thread.start();    
         }
 	}
 }
