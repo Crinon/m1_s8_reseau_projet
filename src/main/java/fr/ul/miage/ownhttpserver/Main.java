0bc0483684b3553d0832dced9d52da63c6589b9c
Nicolas Crinon <48944000+Crinon@users.noreply.github.com>
2021-05-17 22:15:57 +0200

    Ajout fichier propriété

--- src/main/java/fr/ul/miage/ownhttpserver/Main.java
+++ src/main/java/fr/ul/miage/ownhttpserver/Main.java
@@ -1,16 +1,26 @@
 package fr.ul.miage.ownhttpserver;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
 public class Main {
 	
 
-	static final int port = 4003;
+	static final int port = 4005;
 	
 	public static void main(String[] args) throws IOException {
+		
+		Properties prop = new Properties();
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();           
+		InputStream inputStream = loader.getResourceAsStream("properties");
+	    prop.load(inputStream);
+	    System.out.println(prop.getProperty("miniweb.miage"));
+		String resourceMiniweb = prop.getProperty("miniweb.miage");
+		
 		InetAddress host = InetAddress.getByName("0.0.0.0");
     	System.out.println("Main launched with port "+port);
         ServerSocket srv = new ServerSocket(port,1, host);
